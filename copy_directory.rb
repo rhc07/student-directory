7529b17957d180fa2cf816d045bb4bd599bfcf28
Rorie Clarke <Rorie@MacBook-Pro.local>
2021-01-25 19:59:01 +0000

    Made a copy and doing exercises in Step 8

--- copy_directory.rb
+++ copy_directory.rb
@@ -21,8 +21,8 @@ puts  "-------------"
 end
 
 def print(students)
-  students.each.with_index do|student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do|(student), index|
+    puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
