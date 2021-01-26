ea6f5aabf8d7081985ea69bcefb472ebe468ca7c
Rorie Clarke <Rorie@MacBook-Pro.local>
2021-01-26 21:49:14 +0000

    Added cohorts

--- copy_directory.rb
+++ copy_directory.rb
@@ -1,5 +1,22 @@
 
 @width = 75
+@students = []
+
+@cohorts = {
+  January: 0,
+  February: 3,
+  March: 0,
+  April: 3,
+  May: 0,
+  June: 10,
+  July: 20,
+  August: 19,
+  September: 0,
+  October: 2,
+  Novemvber: 1,
+  December: 5
+}
+
 
 def create_student
   puts "Would you like to add a student to the system? (y/n)".center(@width)
@@ -14,29 +31,76 @@ def create_student
   create_student
 end
 
+def add_cohort
+  next_cohort = :April
+  puts "Available cohorts"
+  puts "(Month / Vacancies)"
+  @cohorts.each do |month, vacancies|
+    puts "#{month} - vacancies: #{vacancies}"
+  end
+  puts " "
+  puts "Please enter the month of the cohort you'd like to join:"
+  cohort = gets.chomp
+
+  case cohort
+  when "January"
+    puts "January is full, you have been placed in February cohort."
+    cohort = :February
+  when "February"
+    cohort = :February
+  when "March"
+    puts "March is full, you have been placed in April cohort."
+    cohort = :April
+  when "April"
+    cohort = :April
+  when "May"
+    puts "May is full, you have been placed in June cohort."
+    cohort = :June
+  when "June"
+    cohort = :June
+  when "July"
+    cohort = :July
+  when "August"
+    cohort = :August
+  when "September"
+    puts "September is full, you have been placed in October cohort."
+    cohort = :October
+  when "October"
+    cohort = :October
+  when "November"
+    cohort = :November
+  when "December"
+    cohort = :December
+  else
+    puts "Input not recognised, you have been placed in the #{next_cohort} cohort."
+    cohort = next_cohort
+  end
+cohort
+end
 
 def input_students
 
-  students = []
   continue = create_student
   while continue == true do
 
   puts "Please enter the name of the student".center(@width)
   name = gets.chomp
 
-  puts "please enter the country of birth".center(@width)
+  cohort = add_cohort
+
+  puts "Please enter the country of birth".center(@width)
   country_of_birth = gets.chomp
 
-  puts "please enter student height(cm)".center(@width)
+  puts "Please enter student height(cm)".center(@width)
   height = gets.chomp
 
   hobbies = add_hobbies
 
-  students << { name: name, cohort: :november, country_of_birth: country_of_birth, height: height, hobbies: hobbies}
-     puts "Now we have #{students.count} students".center(@width)
+  @students << { name: name, cohort: cohort.to_sym, country_of_birth: country_of_birth, height: height, hobbies: hobbies}
+     puts "Now we have #{@students.count} students".center(@width)
      continue = create_student
   end
- students
+ @students
 end
 
 def add_hobbies
@@ -60,8 +124,9 @@ end
 
 def print(students)
      index = 0
-     while index < students.count
-       puts "#{index + 1}: #{students[index][:name]}, #{students[index][:country_of_birth]}, (#{students[index][:height]}cm), (#{students[index][:cohort]} cohort), (hobbies: #{students[index][:hobbies]})".center(@width)
+     while index < @students.count
+       puts "#{index + 1}: #{@students[index][:name]}, #{@students[index][:country_of_birth]}, (#{@students[index][:height]}cm), (#{@students[index][:cohort]} cohort), (hobbies: #{@students[index][:hobbies]})".center(@width)
+     index += 1
      end
 end
 
@@ -90,9 +155,9 @@ def print_footer(students)
    puts "Overall, we have #{students.count} great students.".center(@width)
 end
 
-students = input_students
+@students = input_students
 
 print_header
-print(students)
-print_by_character(students)
-print_footer(students)
+print(@students)
+print_by_character(@students)
+print_footer(@students)
