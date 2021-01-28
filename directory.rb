f47d7dcf66d8f2c99def0ab4391a045feb7d51b8
Rorie Clarke <Rorie@MacBook-Pro.local>
2021-01-28 21:39:17 +0000

    Created Command Line App

--- directory.rb
+++ directory.rb
@@ -18,7 +18,7 @@ end
 def interactive_menu
   loop do
     print print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -45,7 +45,7 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  name = gets.chomp
+  name = STDIN.gets.chomp
 
   while !name.empty? do
     @students << {name: name, cohort: :november}
@@ -121,8 +121,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
@@ -130,5 +130,17 @@ def load_students
   file.close
 end
 
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}."
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
 
 interactive_menu
